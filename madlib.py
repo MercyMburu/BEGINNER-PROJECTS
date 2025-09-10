3f6abcdf378e8099c3f5524f6d4c493486ab21c7
Mercy <wmercy160@gmail.com>
2025-09-10 07:33:09 +0300

    madlib commit

--- madlib.py
+++ madlib.py
@@ -1,37 +1,44 @@
-#Goal
+import random
 
-#Create a Python program that asks the user for different types of words (nouns, verbs, adjectives, etc.) and then generates a funny story using their inputs.
+def madlib():
+    print("Hello! Welcome to my Mad libs Game.")
+    print("Enter the following words")
 
-# Requirements
+    noun=input("Enter a noun: ")
+    adjective=input("Enter an adjective: ")
+    verb=input("Enter a verb: ")
+    place=input("Enter the name of a place: ")
+    animal=input("Enter the name of an animal: ")
+    food=input("Enter the name of a type pf food: ")
 
-# Your program should:
+    stories = [
+    "One day, a {adjective} {animal} decided to {verb} at the {place}. Everyone laughed when it grabbed a {noun} and ran away with some {food}.",
 
-# Greet the user.
+    "In the middle of {place}, a {adjective} {animal} tried to {verb} a {noun}. Luckily, someone distracted it with {food}.",
 
-# Ask the user to enter at least 5 different words (e.g., noun, adjective, verb, place, animal, food).
+    "Yesterday, I saw a {adjective} {animal} that wanted to {verb} inside the {place}. Instead, it found a {noun} and ate {food}.",
 
-# Insert those words into a pre-written story.
+    "A {adjective} {animal} once lived in the {place}. It loved to {verb} all day while keeping a {noun} in its pocket and munching on {food}.",
 
-# Print the completed story back to the user.
+    "During my trip to {place}, I met a {adjective} {animal}. It tried to {verb} my {noun}, but I saved it by offering some {food}."
+]
+    # Pick a random template
 
-# Use string formatting (e.g., f-strings or .format()).
-
-# Add at least one line break (\n) in your story to make it look nice.
-
-print("Hello! Welcome to my Mad libs Game.")
-print("Enter the following words")
-
-noun=input("Enter a noun: ")
-adjective=input("Enter an adjective: ")
-verb=input("Enter a verb: ")
-place=input("Enter the name of a place: ")
-animal=input("Enter the name of an animal: ")
-food=input("Enter the name of a type pf food: ")
-
-story = f"""
-    One {adjective} day, I went to the {place}\n
-    I saw a {animal} that tried to {verb} my {noun}.
-    Luckily, I had some {food}, and everything turned out fine!
-    """
+    story_template=random.choice(stories)
     
-print(story)
+    
+    #Format it with user inputs
+    story=story_template.format(
+        adjective=adjective,
+        animal=animal,
+        verb=verb,
+        place=place,
+        noun=noun,
+        food=food
+    )
+    
+    
+    print(story)
+
+if __name__=="__main__":
+    madlib()
